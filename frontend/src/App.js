ba71ab4811197130fa017ad32893971ba61a4a10
KylerBaldwin <kclbald@gmail.com>
2024-10-31 14:24:57 -0500

    added responsive navbar with tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,6 @@ function App() {
   return (
     <div>
       <Navbar/>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
     </div>
   );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 const Navbar = () => {
+    const [nav, setNav] = useState(true)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
     return (
-        <div>
-            <h1>Kyler Baldwin</h1>
+        <div className='flex justify-between items-center h-16 w-full mx-auto px-4 bg-emerald-800 text-white'>
+                <h1 className='w-full text-2xl font-bold'>KYLER BALDWIN</h1>
+                <ul className='hidden md:flex'>
+                    <li className='p-4'>Home</li>
+                    <li className='p-4'>Blog</li>
+                    <li className='p-4'>Projects</li>
+                </ul>
+            <div onClick={handleNav} className='block md:hidden'>
+                {!nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+            </div>
+            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-600 bg-white ease-in-out duration-500' : 'fixed left-[-100%]'}>
+                <h1 className='w-full text-3xl m-4'>Kyler Baldwin</h1>
+                <ul className='p-4'>
+                    <li className='py-4 border-b border-gray-600'>Home</li>
+                    <li className='py-4 border-b border-gray-600'>Blog</li>
+                    <li className='py-4'>Projects</li>
+                </ul>
+            </div>
         </div>
     )
 }
