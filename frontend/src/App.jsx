13e1fd1eba8292a5df9d645a646b30e98901b936
KylerBaldwin <kclbald@gmail.com>
2024-10-31 01:36:54 -0500

    added front end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'; // Import the CSS file
+
+const App = () => {
+  return (
+    <div className="landing-page">
+    <h1>Content is Coming Soon!</h1>
+    <p>We're working hard to bring you new content. Stay tuned!</p>
+    </div>
+  );
+};
+
+export default App;
