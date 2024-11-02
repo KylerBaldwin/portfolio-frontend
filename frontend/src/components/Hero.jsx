9153d3f16c5fa199d06a1e7c6f6511bcf97a0a66
KylerBaldwin <kclbald@gmail.com>
2024-11-01 23:28:10 -0500

    added application under construction

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-      
+    <div className='bg-white'>
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="mx-auto max-w-3xl py-16">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+          </div>
+          <div className="text-center">
+            <h1 className="text-balance text-5xl font-serif tracking-tight text-gray-900 sm:text-7xl">
+              Kyler Baldwin
+            </h1>
+            <p className="mt-8 text-pretty text-lg font-serif font-medium text-gray-500 sm:text-xl/8">
+              My Portfolio Website is currently under construction. Lots of cool things in the works! In the meatime, connect with me on LinkedIn:
+            </p>
+            <div className="flex items-center justify-center gap-x-6">
+              <a
+                href="https://www.linkedin.com/in/kyler-baldwin"
+                className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-white"
+              >
+                <img src="/In-Blue-26-Γö¼┬½@2x.png" alt="" className='pl-2 object-fill' />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,13 +2,12 @@ import React, { useState } from 'react'
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 const Navbar = () => {
     const [nav, setNav] = useState(true)
-
     const handleNav = () => {
         setNav(!nav)
     }
     return (
-        <div className='flex justify-between items-center h-16 w-full mx-auto px-4 bg-emerald-800 text-white'>
-                <h1 className='w-full text-2xl font-bold'>KYLER BALDWIN</h1>
+        <div className='flex justify-between items-center h-16 w-full mx-auto px-4 bg-gray-100 shadow-md'>
+                <h1 className='w-full text-2xl font-semibold'>Kyler Baldwin</h1>
                 <ul className='hidden md:flex'>
                     <li className='p-4'>Home</li>
                     <li className='p-4'>Blog</li>
@@ -17,7 +16,7 @@ const Navbar = () => {
             <div onClick={handleNav} className='block md:hidden'>
                 {!nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
             </div>
-            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-white-600 bg-emerald-800 ease-in-out duration-500 md:hidden' : 'fixed left-[-100%]'}>
+            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-white-600 bg-gray-400 ease-in-out duration-500 md:hidden' : 'fixed left-[-100%]'}>
                 <h1 className='w-full text-3xl m-4'>Kyler Baldwin</h1>
                 <ul className='p-4'>
                     <li className='py-4 border-b border-white-600'>Home</li>
