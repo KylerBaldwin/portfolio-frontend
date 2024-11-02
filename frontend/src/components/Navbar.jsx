2be997da37c585f884c1ff238cebfb80c5f40aae
KylerBaldwin <kclbald@gmail.com>
2024-11-01 21:25:37 -0500

    fixed Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,11 +17,11 @@ const Navbar = () => {
             <div onClick={handleNav} className='block md:hidden'>
                 {!nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
             </div>
-            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-600 bg-white ease-in-out duration-500' : 'fixed left-[-100%]'}>
+            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-white-600 bg-emerald-800 ease-in-out duration-500 md:hidden' : 'fixed left-[-100%]'}>
                 <h1 className='w-full text-3xl m-4'>Kyler Baldwin</h1>
                 <ul className='p-4'>
-                    <li className='py-4 border-b border-gray-600'>Home</li>
-                    <li className='py-4 border-b border-gray-600'>Blog</li>
+                    <li className='py-4 border-b border-white-600'>Home</li>
+                    <li className='py-4 border-b border-white-600'>Blog</li>
                     <li className='py-4'>Projects</li>
                 </ul>
             </div>
